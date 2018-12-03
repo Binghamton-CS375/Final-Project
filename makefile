e7cbc70bfec74267dcf0b1a1611ca15a31f43f6b
vladimirmalcevic@ZANZIBAR <vladimirmalcevic@gmail.com>
2018-12-02 21:33:56 -0500

    Finished Matching Algorithm
    Completed Rabin-Karp string matching algorithm with test in main to
    ensure succesful run of program  and makefile to compile and run
    executable.

---
+++ makefile
@@ -0,0 +1,16 @@
+CC = g++
+CFLAGS = -g -Wall
+EXC = submission
+SRC = rabinkarpalgorithm.cpp
+OBJ = $(SRC:.cpp = .o)
+IN = input.txt
+OUT = output.txt
+
+all: $(OBJ)
+	$(CC) $(CFLAGS) -o $(EXC) $(OBJ)
+
+run: all
+	./$(EXC)
+
+clean:
+	rm -f *.o $(EXC)
--- rabinkarpalgorithm.cpp
+++ rabinkarpalgorithm.cpp
@@ -1,37 +1,105 @@
-/******************************************************************************/
+/*BEGIN IMPORT STATEMENTS******************************************************/
 #include<stdio.h>
 #include<stdlib.h>
+#include<tgmath.h>
 #include<string>
-#include<iostrem>
+#include<iostream>
 #include<fstream>
-#include<string>
+#include<vector>
+/*END IMPORT STATEMENTS********************************************************/
 /******************************************************************************/
-/******************************************************************************/
-/******************************************************************************/
+/*BEGIN NAMESPACE**************************************************************/
 using namespace std;
+/*END NAMESPACE****************************************************************/
 /******************************************************************************/
-/******************************************************************************/
-/******************************************************************************/
+/*BEGIN STRUCTS****************************************************************/
 
+typedef struct{
 
-/******************************************************************************/
-/******************************************************************************/
-/******************************************************************************/
+    vector<int> solution_offsets;
 
+} solution;
 
+/*END STRUCTS******************************************************************/
 /******************************************************************************/
+/*BEGIN PROTOTYPES*************************************************************/
+
+vector<int> RabinKarpMatcher(string text, string pattern, int radix, int prime);
+
+/*END PROTOTYPES***************************************************************/
 /******************************************************************************/
-/******************************************************************************/
+/*START MAIN*******************************************************************/
 int main(int argc, char ** argv, char ** envp){
+
+
+    string text = "VLAD IS HUNGRY AND VLAD IS HAPPY";
+    string pattern = "IS";
+    int radix = 256;
+
+    int prime = 271;
+
+
+    vector<int> solutions = RabinKarpMatcher(text, pattern, radix, prime);
+
+
+    printf("SIZE OF %d\n", int(solutions.size()));
+
+    for(vector<int>::iterator it = solutions.begin(); it != solutions.end(); it++){
+        cout << (*it) << "\t" << text.substr((*it), pattern.length()) << endl;
+
+    }
+
+
     return 0;
 }
+/*END MAIN*********************************************************************/
 /******************************************************************************/
-/******************************************************************************/
-/******************************************************************************/
+/*START FUNCTIONS**************************************************************/
 
+vector<int> RabinKarpMatcher(string text, string pattern, int radix, int prime){
 
+    vector<int> solutions;
+
+    int text_length = text.length();
+    int pattern_length = pattern.length();
+    int h = int(pow(radix, (pattern_length - 1))) % prime;
+    int pattern_value = 0;
+    int text_value = 0;
+
+    for(int i = 0; i < pattern_length; i++){
+
+        pattern_value = (radix * pattern_value + pattern[i]) % prime;           //
+
+        text_value = (radix * text_value + text[i]) % prime;                    //
+
+    }
+
+    // printf("PATTERN VALUE: %d\n\nTEXT VALUE: %d\n\n", pattern_value, text_value);   //debug statement
+
+    for(int s = 0; s <= (text_length - pattern_length); s++){
+
+        if(pattern_value == text_value){
+            // printf("MATCHING PATTERN AND TEXT\n");                              //debug statement
+            if(pattern.compare(text.substr((s),(pattern_length))) == 0){        //comparing pattern to text substring
+                //TODO: offset s is offset in text to find pattern
+                solutions.push_back(s);
+                // printf("%d\n", s);                                              //debug statement
+            }
+        }
+
+        if(s < (text_length - pattern_length)){                                 //if are more windows in text
+            text_value = (radix * (text_value - text[s] * h) + text[s + pattern_length]) % prime;
+
+            if(text_value < 0){
+                text_value = text_value + prime;
+            }
+            // printf("NEW TEXT VALUE: %d\n", text_value);                         //debug statement
+        }
+    }
+
+    return solutions;
 }
 
-/******************************************************************************/
+/*END FUNCTIONS****************************************************************/
 /******************************************************************************/
 /******************************************************************************/
