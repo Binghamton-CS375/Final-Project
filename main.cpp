b4e0dc6312b8d177c264d43c06bbffd6cceada13
Sean <seanbourke16>
2018-12-04 18:56:16 -0500

    continued main

--- main.cpp
+++ main.cpp
@@ -2,19 +2,34 @@
 #include <string>
 #include <vector>
 #include <iterator>
+#include <cstdlib>
 #include "kmpalgorithm.cpp"
 #include "rabinkarpalgorithm.cpp"
-//#include <istreambuf_iterator>
 
 int main(int argc, char ** argv){
   
   std::ifstream file("alice.txt");
   std::string str((std::istreambuf_iterator<char>(file)),std::istreambuf_iterator<char>());
 
-  std::vector<iteration> data;
-  data=KMP("a",str);
-  std::cout << data[0].time << std::endl;
-  data=RabinKarp("a",str);
-  std::cout << data[0].time << std::endl;
+  std::vector<std::string> words={"the","it","alice","rabbit","this","that","hat","queen","green","things","fghjk","sea","when","go","hello","cake","food","tall","happy","hungry"};
+  std::vector<std::string> sentences={"Would the fall NEVER come to an end!","She went on growing, and growing, and very soon had to kneel down on the floor","Perhaps it hasn't one","Off with her head","I don't know what"};
+  int time1=0;
+  int x;
+  std::string letter="a";
+  for(x=0;x<50;x++){
+    letter[0]='a'+ rand()%26;
+    time1+=KMP(letter,str)[0].time;
+  }
+  std::cout << "KMP single letter search average time: "<< time1/50 << std::endl;
+  time1=0;
+  for(x=0;x<50;x++){
+    time1+=KMP(words[rand()%20],str)[0].time;
+  }
+  std::cout << "KMP word search average time: "<< time1/50 << std::endl;
+  time1=0;
+  for(x=0;x<50;x++){
+    time1+=KMP(sentences[rand()%5],str)[0].time;
+  }
+  std::cout << "KMP sentence search average time: "<< time1/50 << std::endl;
 }
 			       
